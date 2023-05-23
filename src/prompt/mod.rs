ee773e01646b2b91da491419d05eff3e6dd7b3e1
imlyzh <enterindex@gmail.com>
2023-05-23 08:18:34 +0800

    try add prompt

---
+++ src/prompt/mod.rs
@@ -0,0 +1,29 @@
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Serialize, Deserialize)]
+pub struct Prompt {
+  pub project:  Option<String>,
+  pub author:   Option<String>,
+  pub license:  Option<String>,
+  pub version:  Option<String>,
+  pub conf: Option<Config>,
+  pub vars: Option<Vec<Type>>,
+  pub prompts: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+  provider: Option<String>,
+  model: Option<String>,
+  temperature: Option<String>,
+  stop: (String, String),
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Serialize, Deserialize)]
+pub struct Type {
+
+}
