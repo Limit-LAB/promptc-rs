2ecb880238d9b33386bdbe373f777ad9cd07863e
imlyzh <enterindex@gmail.com>
2023-05-23 17:07:32 +0800

    fixed parse bug

--- examples/exam1.rs
+++ examples/exam1.rs
@@ -2,6 +2,6 @@ fn main() {
   use std::str::FromStr;
 
   use promptc::block::Unit;
-  let unit: Result<Unit, _> = Unit::from_str("{%Q%}");
+  let unit: Result<Unit, _> = Unit::from_str("{%");
   println!("{:?}", unit);
 }
