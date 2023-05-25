aa221b7b140f19d15a772d6b3c9c15a3a66dc0d6
imlyzh <enterindex@gmail.com>
2023-05-25 16:07:57 +0800

    writen prompt file serde model

--- src/prompt/mod.rs
+++ src/prompt/mod.rs
@@ -1,6 +1,7 @@
+
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq)]
 #[derive(Serialize, Deserialize)]
 pub struct Prompt {
   pub project: Option<String>,
@@ -18,9 +19,19 @@ pub struct Config {
   provider:    Option<String>,
   model:       Option<String>,
   temperature: Option<String>,
-  stop:        (String, String),
+  stop:         Option<Vec<String>>,
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq)]
 #[derive(Serialize, Deserialize)]
-pub struct Type {}
+pub struct Type (pub String, pub TypeLimit);
+
+#[allow(non_snake_case)]
+#[derive(Debug, PartialEq)]
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum TypeLimit {
+  Int{min: Option<u64>, max: Option<u64>, default: Option<String>},
+  Float{min: Option<f64>, max: Option<f64>, default: Option<String>},
+  String{minLen: Option<usize>, maxLen: Option<usize>, default: Option<String>}, // fixme: replace usize to u64
+}
